0d2bc1c07a86bd69095888da2b7d55610b95d44d
Kai Orion <bre97.web@gmail.com>
2026-04-30 13:24:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# @sandlada/consts.js
+
+![npm version](https://img.shields.io/npm/v/@sandlada/consts.js?label=NPM%20Version&labelColor=%2300531f&color=%23a3f5aa)
+![GitHub License](https://img.shields.io/github/license/sandlada/consts.js?label=License&labelColor=%2300531f&color=%23a3f5aa)
+
+For private use only.
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+
+export const Consts = {
+  domain: {
+    MaterialDesignCSS: `https://material-design-css.sandlada.com`,
+    MDC: `https://mdc.sandlada.com`,
+    Blog: `https://blog.sandlada.com`,
+    KaiOrion: `https://kai-orion.sandlada.com`,
+    Sandlada: `https://sandlada.com`,
+  },
+  member: {
+    kaiOrion: {
+      name: `kai-orion`,
+      formalName: `Kai Orion`,
+      githubProfileLink: `https://github.com/kai-orion`,
+      npmProfileLink: `https://www.npmjs.com/~kai-orion`,
+      offcialWebLink: `https://kai-orion.sandlada.com`
+    },
+    sandlada: {
+      name: `sandlada`,
+      singleFormalName: `Sandlada`,
+      formalName: `Kai-Orion & Sandlada`,
+      githubProfileLink: `https://github.com/sandlada`,
+      npmProfileLink: `https://www.npmjs.com/org/sandlada`,
+    },
+  },
+  repo: {
+    MaterialDesignCSS: {
+      author: `Kai-Orion & Sandlada`,
+      formalName: `@sandlada/material-design-css`,
+      githubName: `material-design-css`,
+      githubLink: `https://github.com/sandlada/material-design-css`,
+      npmName: '@sandlada/material-design-css',
+      npmLink: `https://www.npmjs.com/package/@sandlada/material-design-css`,
+      npmDownload: `npm i @sandlada/material-design-css`,
+      licenseLink: `https://github.com/sandlada/material-design-css/blob/main/LICENSE`,
+      licenseName: `MIT`,
+    },
+    MDC: {
+      author: `Kai-Orion & Sandlada`,
+      formalName: `@sandlada/mdc`,
+      githubName: `mdc`,
+      githubLink: `https://github.com/sandlada/mdc`,
+      npmName: '@sandlada/mdc',
+      npmLink: `https://www.npmjs.com/package/@sandlada/mdc`,
+      npmDownload: `npm i @sandlada/mdc`,
+      licenseLink: `https://github.com/sandlada/mdc/blob/main/LICENSE`,
+      licenseName: `MIT`,
+    },
+  },
+  social: {
+    twitter: {
+      link: `https://x.com/Sandlada_team`,
+    },
+    github: {
+      link: `https://github.com/sandlada`,
+    },
+    npm: {
+      link: `https://www.npmjs.com/org/sandlada`,
+    },
+  }
+} as const
